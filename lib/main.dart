4cb0be47c64dba0c059067d4339dc37ec9684da4
3S Monteiro <monteiroblm@fab.mil.br>
2021-07-20 14:50:26 -0300

    Added theme data

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:wallstreetbets/app/bindings/home_binding.dart';
 import 'package:wallstreetbets/app/routes/app_pages.dart';
 import 'package:wallstreetbets/app/routes/app_routes.dart';
 import 'package:wallstreetbets/app/ui/home_page.dart';
+import 'package:wallstreetbets/app/ui/theme/app_theme.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: appThemeData,
       initialRoute: AppRoutes.INITIAL,
       initialBinding: HomeBinding(),
       getPages: AppPages.routes,
